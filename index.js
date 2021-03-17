1dcec02e57b366d72d6bde9f0e07471e7c5fad77
robertvorlicsek <plavins@gmail.com>
2021-03-17 18:52:43 +0100

    second commit

--- index.js
+++ index.js
@@ -39,10 +39,10 @@ app.use('/api/users', usersRoutes);
 
 // app.use(express.static(path.join('frontend')));
 
-app.use((req, res, next) => {
-  const error = new HttpError('Could not find this route', 404);
-  throw error;
-});
+// app.use((req, res, next) => {
+//   const error = new HttpError('Could not find this route', 404);
+//   throw error;
+// });
 
 app.use((error, req, res, next) => {
   if (req.file) {
